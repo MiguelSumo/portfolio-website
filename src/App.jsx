23b037ad779bb25b42664113ffc752be5c598c1e
MiguelSumo <jj9101dy@go.minnstate.edu>
2026-02-18 20:17:38 -0600

    Completed Navbar + Hero Section

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { Navbar } from "@/layout/Navbar"
+import { Hero } from "@/sections/Hero"
+import { About } from "@/sections/About"
+import { Projects } from "@/sections/Projects"
+import { Experience } from "@/sections/Experience"
+import { References } from "@/sections/References"
+import { Contact } from "@/sections/Contact"
+
+
+function App() {
+  return( 
+  <div className="min-h-screen overflow-x-hidden">
+          <Navbar />
+      <main>
+          <Hero />
+          <About />
+          <Projects />
+          <Experience />
+          <References />
+          <Contact />
+      </main>
+  </div>
+  );
+}
+
+export default App;
