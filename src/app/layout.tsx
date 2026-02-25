6b6319d2ba804cac09b9f3e823a654be020c9151
MiguelSumo <jj9101dy@go.minnstate.edu>
2026-02-25 14:14:19 -0600

    google metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,21 @@ export const metadata = {
   description: "Web Developer and Designer Portfolio",
   keywords: "web development, web design, portfolio, fullstack developer",
   authors: [{ name: "Miguel Sumo" }],
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
   openGraph: {
     title: "Miguel Sumo | Portfolio",
     description: "Web Developer and Designer Portfolio",
     type: "website",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
   },
 };
 
